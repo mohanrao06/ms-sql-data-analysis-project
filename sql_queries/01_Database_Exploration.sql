ff08dd7fe944cc3f3e7fec57c3ff4145c78cbfdb
Mohan rao <gedalamohanraolpu@gmail.com>
2026-02-07 23:56:47 +0530

    explored full data

---
+++ sql_queries/01_Database_Exploration.sql
@@ -0,0 +1,8 @@
+-- Explore All Objects in the Database
+
+SELECT * FROM INFORMATION_SCHEMA.TABLES
+
+
+-- Explore All Columns in the Database
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME='dim_customers'
