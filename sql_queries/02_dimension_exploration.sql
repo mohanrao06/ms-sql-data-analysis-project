a5407a00b3fbfb0bae79cf4b5125f0607cc51b1b
Mohan rao <gedalamohanraolpu@gmail.com>
2026-02-08 19:10:17 +0530

    explored about all colums

---
+++ sql_queries/02_dimension_exploration.sql
@@ -0,0 +1,7 @@
+--Explore All Countries our Customers come from.
+
+SELECT DISTINCT country from gold.dim_customers
+
+
+--Explore All Categories "The major Divisions"
+SELECT DISTINCT category,subcategory,product_name from gold.dim_products
